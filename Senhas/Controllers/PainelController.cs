a69772c2e9288ca9abc9dc0e4d8e9eca01c0ea7e
WILLIAN <WILLIAN@2RIPCDEV12025>
2026-01-08 11:35:58 -0300

    Task: Tempo medio de espera no painel

--- Senhas/Controllers/PainelController.cs
+++ Senhas/Controllers/PainelController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Senhas.Models.Entities;
 using Senhas.Models.Enums;
+using System.Linq;
 
 namespace Senhas.Controllers
 {
@@ -16,12 +17,29 @@ namespace Senhas.Controllers
 
         public IActionResult Index()
         {
+            // Últimas 5 senhas chamadas ou finalizadas
             var chamadas = _context.Senhas
                 .Include(s => s.Guiche)
                 .Where(s => s.Status == StatusSenha.EmAtendimento || s.Status == StatusSenha.Finalizada)
                 .OrderByDescending(s => s.DataChamada)
                 .Take(5)
                 .ToList();
+
+            // Calcula tempo médio de espera (em minutos) de todas as senhas chamadas
+            var senhasChamadas = _context.Senhas
+                .Where(s => s.DataChamada != null)
+                .ToList();
+
+            double tempoMedio = 0;
+
+            if (senhasChamadas.Any())
+            {
+                tempoMedio = senhasChamadas
+                    .Average(s => (s.DataChamada.Value - s.DataCriacao).TotalMinutes);
+            }
+
+            ViewBag.TempoMedioEspera = $"{Math.Round(tempoMedio)} minutos";
+
 
             return View("~/Views/Painel/Index.cshtml", chamadas);
         }
